acc4e2a9e08a08183e742a3fa58762c3f47713f2
zuiwoshachangjunmoxiao <smilegks@163.com>
2017-09-19 14:57:29 +0800

    第二次提交代码，测试新建的累

---
+++ src/SeleniumProj/GitTest.java
@@ -0,0 +1,14 @@
+package SeleniumProj;
+/** 
+* @author 作者：BarryAllen
+* @version 创建时间：2017年9月19日 下午2:56:48 
+* 类说明 
+*/
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
