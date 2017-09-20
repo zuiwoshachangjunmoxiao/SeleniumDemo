403a0526f9f1ab71ba3b341180b5120488d16b18
zuiwoshachangjunmoxiao <smilegks@163.com>
2017-09-20 15:05:30 +0800

    添加了selenium的getPageSource()方法获取页面源码。

---
+++ src/SeleniumProj/GetPageSource.java
@@ -0,0 +1,38 @@
+package SeleniumProj;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/** 
+* @author 作者：BarryAllen
+* @version 创建时间：2017年9月20日 上午11:07:53 
+* 类说明 
+*/
+public class GetPageSource {
+	WebDriver driver;
+	String baseUrl = "http://10.10.192.94:8082/fuhe24-collection";
+	
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void test() throws Exception {
+		driver.get(baseUrl);
+		String pagesource1 = driver.getPageSource();
+		System.out.println(pagesource1);
+		Thread.sleep(5000);
+	}
+
+}
---
+++ src/SeleniumProj/GetPageSourceJava.java
@@ -0,0 +1,27 @@
+package SeleniumProj;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/** 
+* @author 作者：BarryAllen
+* @version 创建时间：2017年9月20日 上午11:14:26 
+* 用selenium的getPageSource方法获取页面的源码，在Java的main方法中实现。
+*/
+public class GetPageSourceJava {
+	
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		String baseUrl = "http://10.10.192.94:8082/fuhe24-collection";
+		driver = new FirefoxDriver();
+		//打开网页
+		driver.get(baseUrl);
+		//获取源码
+		String pageSource1 = driver.getPageSource();
+		System.out.println(pageSource1);
+		
+
+	}
+
+}
